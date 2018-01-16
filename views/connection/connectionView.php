7ba6834778fc232fa9d4dd150d247ef306b80e22
thomas-soutif <th.soutif@gmail.com>
2018-01-16 09:59:00 +0100

    La page de connexion a subit un second souffle !

--- views/connection/connectionView.php
+++ views/connection/connectionView.php
@@ -1,29 +1,40 @@
 <section id="corps">
 
-    <section id="main-page-center">
-        <div id="bar-account">
+
+    <section id="pannel-register">
+        <br><br><br><br><br><br><br>
+        <strong>Pas de compte ?</strong>
+        <br><br>
+        <div class="button-valid-3">
+            <a href="">Créer un compte</a>
+        </div>
+
+    </section>
+
+    <section id="main-page">
+        <div id="bar-account-2">
             <h1>Connexion</h1>
         </div>
         <br><br>
-        <div id="form-inscription-co">
+        <div id="form-connect">
             <form action="../connection/validate" method = "post">
-                <label for="email">Adresse email</label></br>
-                <input type="text" name="email" required/>  </br></br>
-                <label for="mdp">Mot de passe</label></br>
-                <input type="password" name="mdp" required/> </br></br>
-                <input type="submit" class="button-connexion" name="" value="Se connecter" /></br></br>
-                <a href="/connection/impossible">Impossible de se connecter ?</a>
+                <div class="form-recovery-block">
+                    <label for="email">Adresse email</label></br>
+                    <input type="text" name="email" required/>  </br></br>
+                    <?php
+                    if(isset($_SESSION['error_connexion'])) echo '<br>' . $_SESSION['error_connexion']; unset($_SESSION['error_connexion']); ?>
+                </div>
+                <div class="form-recovery-block">
+                    <label for="mdp">Mot de passe</label></br>
+                    <input type="password" name="mdp" required/> </br></br>
+                </div>
+                <div class="form-recovery-block-2">
+                    <input type="submit" class="button-valid-2" name="" value="Se connecter" /></br></br>
+                    <a href="/connection/impossible">Impossible de se connecter ?</a>
+                </div>
+
             </form>
-            <?php
-                if(isset($_SESSION['error_connexion']) || !empty($_SESSION['error_connexion']))
-                {
-                    echo '<br>' . $_SESSION['error_connexion'];
-                    unset($_SESSION['error_connexion']);
-                }
-            ?>
+
         </div>
     </section>
 </section>
-
-
-
