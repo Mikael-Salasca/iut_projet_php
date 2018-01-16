98a5732c454f763f5ae50217fa7182cc06e183ba
thomas-soutif <th.soutif@gmail.com>
2018-01-16 18:40:16 +0100

    nouveau design pour la page d'identification de mot de passe perdue

--- views/forgotpass/recoverypassView.php
+++ views/forgotpass/recoverypassView.php
@@ -17,13 +17,20 @@
     </section>
 
     <section id="main-page">
+        <div id="block-row">
+            <ul class="ak-stepper-list">
+                <li class="current">Identification</li>&nbsp;&nbsp;&nbsp;&nbsp;----------------------&nbsp;&nbsp;&nbsp;&nbsp;
+                <li>Boite email</li>&nbsp;&nbsp;&nbsp;&nbsp;----------------------&nbsp;&nbsp;&nbsp;&nbsp;
+                <li>Modification</li>
 
+            </ul>
+        </div>
 
-        <br><br>
+        <br>
         <div id="bar-account">
             <h1>Identifiez le compte concerné</h1>
             <?php if(isset($_SESSION['error_system'])) echo $_SESSION['error_system']; unset($_SESSION['error_system']); ?>
-        </div><br><br>
+        </div>
         <div id="form-recovery-pass">
             <form action="/forgotpass/recoverypass" method = "post">
                 <div class="form-recovery-block">
