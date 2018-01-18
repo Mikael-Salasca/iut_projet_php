876855a452f9ed87e5c9fdc1e8607d4adac7443e
Vincent <vincent.weber@etu.univ-amu.fr>
2018-01-18 16:17:15 +0100

    requete pour récupérer les users

---
+++ model/userinfo.php
@@ -0,0 +1,30 @@
+<?php
+
+if (!class_exists('UsersDataBase'))
+{
+    require ROOT . '/model/base.php';
+}
+
+function getAllUsersInfo() {
+    $usersDataBase = new UsersDataBase();
+    $dbConnection = $usersDataBase->dbConnect();
+
+    $query = 'SELECT NAME, EMAIL, TYPEACCOUNT FROM user';
+    $stmt = $dbConnection->prepare($query);
+    try {
+        $stmt->execute();
+        if ($stmt->rowCount()) {
+            $stmt->setFetchMode(PDO::FETCH_OBJ);
+            $result = $stmt->fetchAll();
+            return $result;
+        }
+        else {
+            return false;
+        }
+    }
+    catch (PDOException $e) {
+        echo 'Erreur : ', $e->getMessage(), PHP_EOL;
+        echo 'Requête : ', $query, PHP_EOL;
+        exit();
+    }
+}
