f306967d63104e872dd8bd22c1dd7d6ca4b0d263
s16008030 <mikael.salasca@etu.univ-amu.fr>
2018-01-22 14:17:44 +0100

    traduction vue confirmation

--- views/account/viewConfirmation.php
+++ views/account/viewConfirmation.php
@@ -9,7 +9,7 @@
             <br><br>
             <?php echo translate('Ainsi vos informations sont')?> <strong><?php echo translate('protégés')?>.</strong><br><br>
             <strong><?php echo translate('Le code de vérification expirera au bout de 30 minutes. Si d\'ici là vous n\'avez pas eu le temps d\'accèder à votre boite email')?>,
-            <strong> <strong><?php echo translate('pas de panique')?>.</strong> Recommencez l\'opération depuis votre gestion de compte.
+            <strong> <strong><?php echo translate('pas de panique')?>.</strong> <?php echo translate('Recommencez l\'opération depuis votre gestion de compte')?>.
         </div>
 
 
@@ -18,9 +18,9 @@
     <section id="main-page">
         <div id="block-row">
             <ul class="ak-stepper-list">
-                <li>Email actuelle</li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <li class="current">Code de sécurité</li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <li>Nouvelle adresse email</li>
+                <li><?php echo translate('Email actuel')?></li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <li class="current"><?php echo translate('Code de sécurité')?></li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <li><?php echo translate('Nouvelle adresse email')?></li>
             </ul>
         </div>
 
@@ -31,19 +31,19 @@
         </div>
 
         <div class="block-12">
-            Un e-mail vient de vous être envoyé
+            <?php echo translate('Un e-mail vient de vous être envoyé')?>
         </div>
         <div class="id1">
-            <p>Pour continuer, vous devez saisir le code de sécurité qui vient d'être envoyé sur votre adresse email<br> <strong><?php echo $_SESSION['crypt_email']; ?></strong></p>
+            <p><?php echo translate('Pour continuer, vous devez saisir le code de sécurité qui vient d\'être envoyé sur votre adresse email')?><br> <strong><?php echo $_SESSION['crypt_email']; ?></strong></p>
         </div>
         <br>
         <div class="ak-block-mail">
             <form action="/account/verify_code" method="post">
-                <label for="code"> Code de sécurité * </label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <label for="code"> <?php echo translate('Code de sécurité')?> * </label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                 <input type="text" name="code" required maxlength="6">
                 <?php if(isset($_SESSION['wrong_code'])) echo $_SESSION['wrong_code']; unset($_SESSION['wrong_code']); ?>
                 <div class="form-recovery-block-3">
-                    <input type="submit" class="button-valid" name="" value="VALIDER" />
+                    <input type="submit" class="button-valid" name="" value="<?php echo translate('VALIDER')?>" />
                 </div>
             </form>
         </div>
