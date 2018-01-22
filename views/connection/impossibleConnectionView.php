1d390bda9fc5fb6d30a70b49007ad190f1b536a1
s16008030 <mikael.salasca@etu.univ-amu.fr>
2018-01-22 16:29:46 +0100

    debut traduction connection impossible

--- views/connection/impossibleConnectionView.php
+++ views/connection/impossibleConnectionView.php
@@ -3,7 +3,7 @@
     <section id="main-page-large">
         <br><br>
         <div class="impossible-connection">
-            <h1>Vous n'arrivez pas à vous connecter ?</h1>
+            <h1><?php echo translate('Vous n\'arrivez pas à vous connecter')?> ?</h1>
         </div><br>
         <p class="impossible-connection">Aidez-nous à vous aider !<br>
             Indiquez-nous quel est votre souci pour vous connecter</p>
