f05172ed861f50968559558b61522880e60a8dbc
thomas-soutif <th.soutif@gmail.com>
2018-01-23 14:37:46 +0100

    nouveau design pour la page 'mes demandes premiums'

--- views/premium/my_requestView.php
+++ views/premium/my_requestView.php
@@ -1,38 +1,45 @@
 <section id="corps">
 
+
+
     <section id="main-page-large">
+        <div class="card-header">
+            <h2>Mes demandes premiums</h2>
+        </div>
+        <div class="row-compte">
+            <table class="prenium-tab">
 
-        <table style="width: 100%;border: solid 1px;">
+                <tr class="prenium-info-tab">
+                    <td>Ma demande</td>
+                    <td>Langue source</td>
+                    <td>Langue destination</td>
+                    <td>Statut</td>
+                </tr>
 
-            <tr>
-                <td>Mot ou phrase</td>
-                <td>Langue source</td>
-                <td>Langue destination</td>
-                <td>Statut</td>
-            </tr>
+                <?php
+                $i = 0;
+                if (!empty($all_request)) {
+                    foreach ($all_request as $object) {
 
-            <?php
-            if(!empty($all_request)) {
-                foreach ($all_request as $object) {
-                    echo '<tr>';
-                    echo '<td style="width: 50%">' . $object->getDataSource() . '</td>';
-                    echo '<td>' . translate($object->getLangSource(), 'ENGLISH') . '</td>';
-                    echo '<td>' . translate($object->getLangDestination(), 'ENGLISH') . '</td>';
-                    echo '<td>' . translate($object->getStatus(), 'ENGLISH') . '</td>';
-                    echo '</tr>';
+                        if ($i % 2) {
+                            echo '<tr class="c-24">';
+                        } else {
+                            echo '<tr class="c-23">';
+                        }
+                        $i++;
+                        echo '<td>' . $object->getDataSource() . '</td>';
+                        echo '<td>' . translate($object->getLangSource(), 'ENGLISH') . '</td>';
+                        echo '<td>' . translate($object->getLangDestination(), 'ENGLISH') . '</td>';
+                        echo '<td><div class="'. $object->getStatus() .'-requet"   </div>' . translate($object->getStatus(), 'ENGLISH') . '</td>';
+                        echo '</tr>';
+                    }
                 }
-            }
-        ?>
+                ?>
 
 
-        </table>
+            </table>
 
-
-
-
-
-
-
+        </div>
 
 
     </section>
