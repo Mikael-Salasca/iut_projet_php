d6efb01c6d3fc6dfc95bb075abb47ad6235825c1
thomas-soutif <th.soutif@gmail.com>
2018-01-23 15:29:27 +0100

    Nouveau design pour le changement de traductions existantes

--- views/translator/translatorExistingView.php
+++ views/translator/translatorExistingView.php
@@ -1,71 +1,70 @@
-<p> Modifier les traductions existantes.<br>
-    (Pense à cocher les traductions pour confirmer leur modifications)
-</p>
+<div class="card-header">
+    <h2>Traductions existantes</h2>
+</div>
 <a href="/translator/change_control">Acceder aux demandes</a>
 
 <br>
 
+<div class="row-compte">
+
+    <table id="panel-admin">
+
+        <form method="post" action="/translator/change_lang">
+            <tr class="info-table">
+                <td>
+                    <select class="select-admin" name="lgSource">
+                        <?php foreach ($all_langues as $lang) {
+                            $option = '<option value="' . $lang . '" ';
+                            if ($lang == $source) $option .= 'selected="' . $lang . '"';
+                            $option .= '">' . translate($lang, 'ENGLISH') . '</option>';
+                            echo $option;
+
+                        } ?>
+
+                    </select>
 
 
-<table style="color: black; border: solid;width: 100%">
-    <form method="post" action="/translator/change_lang">
-        <tr>
-            <td>
-                <select name="lgSource">
-                    <?php foreach ($all_langues as $lang)
-                    {
-                        $option = '<option value="'. $lang . '" ';
-                        if($lang == $source) $option .= 'selected="' . $lang . '"';
-                        $option .= '">' . translate($lang,'ENGLISH') . '</option>';
-                        echo $option;
+                </td>
+                <td>
+                    <select class="select-admin" name="lgTarget">
+                        <?php
+                        foreach ($all_langues as $lang) {
+                            $option = '<option value="' . $lang . '" ';
+                            if ($lang == $target) $option .= 'selected="' . $lang . '"';
+                            $option .= '">' . translate($lang, 'ENGLISH') . '</option>';
+                            echo $option;
 
-                    } ?>
+                        }
+                        ?>
 
-                </select>
+                    </select>
 
-
-
-            </td>
-            <td>
-                <select name="lgTarget">
-                    <?php
-                    foreach ($all_langues as $lang)
-                    {
-                        $option = '<option value="'. $lang . '" ';
-                        if($lang == $target) $option .= 'selected="' . $lang . '"';
-                        $option .= '">' . translate($lang,'ENGLISH') . '</option>';
-                        echo $option;
-
-                    }
-                    ?>
-
-                </select>
-
-            <td>
-                <input type="submit" value="Appliquer le choix des langues" </td>
+                <td>
+                    <input type="submit" class="button-50" value="Appliquer le choix des langues"</td>
 
 
-            </td>
+                </td>
 
-        </tr>
-    </form>
-    <form method="post" action="/translator/update_translation">
-        <?php foreach ($allTuple as $objWord) {
+            </tr>
+        </form>
+        <form method="post" action="/translator/update_translation">
+            <?php foreach ($allTuple as $objWord) {
 
-            echo '<tr>';
-            echo '<td><textarea style="width: 100%" name ="textareaSource['. $objWord->getId() . '][' . $objWord->getLangSource() . ']">'. $objWord->getDataSource() . '</textarea>';
-            echo '<td COLSPAN=2><textarea style="width: 100%" name ="textareaTarget['. $objWord->getId() . '][' . $objWord->getLangDestination() . ']">'. $objWord->getDataDestination() .'</textarea>';
-            echo '<td><input type="checkbox" name="checkbox[' .$objWord->getId() . ']" value="' . $objWord->getId() . '">';
-            echo '</tr>';
+                echo '<tr>';
+                echo '<td><textarea class="area-trad trad-source" name ="textareaSource[' . $objWord->getId() . '][' . $objWord->getLangSource() . ']">' . $objWord->getDataSource() . '</textarea>';
+                echo '<td COLSPAN=2><textarea class="area-trad trad-target" name ="textareaTarget[' . $objWord->getId() . '][' . $objWord->getLangDestination() . ']">' . $objWord->getDataDestination() . '</textarea>';
+                echo '<td><input type="checkbox" name="checkbox[' . $objWord->getId() . ']" value="' . $objWord->getId() . '">';
+                echo '</tr>';
 
-        }
+            }
 
-        ?>
+            ?>
 
 
-
-</table>
+    </table>
+</div>
 
 <input type="submit" value="envoyer">
 </form>
-<?php if(isset($_SESSION['update_translation_msg'])) echo $_SESSION['update_translation_msg']; unset($_SESSION['update_translation_msg']); ?>
+<?php if (isset($_SESSION['update_translation_msg'])) echo $_SESSION['update_translation_msg'];
+unset($_SESSION['update_translation_msg']); ?>
