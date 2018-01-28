71c7e505dd31bc36709263032b77f785a4fc39bc
Mikael-Salasca <mikael.salasca@etu.univ-amu.fr>
2018-01-28 20:15:19 +0100

    vue mentions légales

--- views/inscription/cuView.php
+++ views/inscription/cuView.php
@@ -2,9 +2,11 @@
 
     <section id="main-page">
         <div id="bar-account">
-            <h1><?php echo translate('CONDITIONS D\'UTILISATION');?></h1>
+            <h1><?php echo translate('Mentions légales et conditions d\'utilisations');?></h1>
         </div>
-        <p><?php echo translate('Pas encore disponible.');?></p>
-    </section>
+        <p><?php echo translate('Editeurs');?> : Thomas SOUTIF (thomas.soutif@etu.univ-amu.fr) , Vincent WEBER(vincent.weber@etu.univ-amu.fr), SALASCA Mikaël(mikael.salasca@etu.univ-amu.fr) </p>
+        <p><?php echo translate('Hébergeur');?> : Alwaysdata (https://www.alwaysdata.com)</p>
+        <p><?php echo translate('Vos données personnelles resteront confidentielles, de plus nous n\'utilisons pas de cookies');?></p>
 
+    </section>
 </section>
